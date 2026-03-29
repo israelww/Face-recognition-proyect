6bb09961e368624384ee497634fe1af9c1d17a40
KevinSamano3321 <L22170815@culiacan.tecnm.mx>
2026-03-28 22:36:25 -0700

    Cantates y presidentes

--- Face-Recognition Proyect/Scripts/Captura.py
+++ Face-Recognition Proyect/Scripts/Captura.py
@@ -5,7 +5,7 @@ import uuid
 ## DBD
 # ─── 1. Configuración ──────────────────────────────────────────────────────────
 nombre_persona = 'Kevin'
-path_datos     = r'C:\Users\Kexis\OneDrive\Desktop\Dataset\Face-recognition-proyect\Face-Recognition Proyect\Dataset\Alumnos'
+path_datos     = r'.\Face-Recognition Proyect\Dataset\Alumnos'  # Cambia a tu ruta deseada
 path_completo  = os.path.join(path_datos, nombre_persona)
 META           = 120      # Número de fotos objetivo
 TAMANO         = (160, 160)  # FIX: 160x160 en lugar de 60x60
